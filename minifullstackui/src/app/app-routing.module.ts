daba4e08d3a045ee378a53868a423a005002c75d
Motjiang <motjiangbrilliant@gmail.com>
2024-11-17 16:55:40 +0200

    Added Product path

--- minifullstackui/src/app/app-routing.module.ts
+++ minifullstackui/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddProductComponent } from './components/add-product/add-product.component';
+import { ListProductsComponent } from './components/list-products/list-products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'add-product',
+    component:AddProductComponent
+  },
+  {
+    path:'update-product/:id',
+    component:AddProductComponent},
+  {
+    path:'list-products',
+    component:ListProductsComponent
+  },
+  {
+    path:'',
+    redirectTo:'/list-products',
+    pathMatch:'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- minifullstackui/src/app/app.module.ts
+++ minifullstackui/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddProductComponent } from './components/add-product/add-product.component';
+import { ListProductsComponent } from './components/list-products/list-products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddProductComponent,
+    ListProductsComponent
   ],
   imports: [
     BrowserModule,
